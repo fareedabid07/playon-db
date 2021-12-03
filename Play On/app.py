da7e257278c897e688c9a3a1f9b396f175fecc03
Mustafa Afzal <78599460+Mafzii@users.noreply.github.com>
2021-12-03 19:29:32 +0500

    Added 3 more use cases
    -added remove video and ban features for admins
    -added like feature for users
    -need to implement comments

--- Play On/app.py
+++ Play On/app.py
@@ -7,7 +7,7 @@ from flask.helpers import flash
 from flask_mysqldb import MySQL
 from werkzeug.utils import redirect, secure_filename
 import yaml
-from datetime import date
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -72,12 +72,10 @@ def login():
                 session['logged_in'] = True
                 session['login_id'] = login_details['login_id']
                 session['is_admin'] = False
-                result = cur.execute("SELECT * FROM clients WHERE login_id = %s", [login_id])
-                if result > 0:
-                    details = cur.fetchone()
-                    session['client_id'] = details['client_id']
+                session['client_id'] = details['client_id']
 
                 return redirect(url_for('homepage'))
+
         else:
             flash('Incorrect password or id', 'message')
             return render_template('login.html')
@@ -140,11 +138,21 @@ def logout():
 @app.route('/home')
 @check_logged_in
 def homepage():
-    return render_template('home.html')
+    cur = mysql.connection.cursor()
 
-@app.route('/add_video', methods=['POST'])
+    result = cur.execute("SELECT * FROM videos ORDER BY upload_date DESC")
+    videos = cur.fetchall()
+
+    if result > 0:
+        return render_template('home.html', videos=videos)
+    else:
+        flash('No videos to display', 'message')
+        cur.close()
+        return render_template('home.html')
+
+@app.route('/upload_video', methods=['POST'])
 @check_logged_in
-def add_video(): 
+def upload_video(): 
     if 'file' not in request.files:
         flash('No file')
         return redirect('/home')
@@ -155,25 +163,92 @@ def add_video():
 
     title = request.form
     title = title['title']
-    client_id = session['client_id']
-    date_uploaded = date.today()
+    uploader_id = session['client_id']
+    date_uploaded = datetime.now()
+    login_id = session['login_id']
 
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO videos(title, client_id, upload_date) VALUES (%s, %s, %s)", (title, client_id, date_uploaded))
+    cur.execute("INSERT INTO videos(title, uploader_id, upload_date, filename, login_id) VALUES (%s, %s, %s, %s, %s)", (title, uploader_id, date_uploaded, filename, login_id))
     mysql.connection.commit()
     cur.close()
 
-    return render_template('home.html', filename=filename)
+    return render_template('profile.html', filename=filename)
 
 @app.route('/display_video/<filename>')
 @check_logged_in
 def display_video(filename):
     return redirect(url_for('static', filename='/uploads'+filename), code=301)
 
+@app.route('/video/<video_id>')
+@check_logged_in
+def video(video_id):
+    cur = mysql.connection.cursor()
+
+    result = cur.execute("SELECT * FROM videos WHERE video_id = %s", [video_id])
+    video_details = cur.fetchone()
+    cur.close()
+    
+    return render_template('video.html', video_details=video_details)
+
 @app.route('/profile/<login_id>')
 @check_logged_in
 def profile(login_id):
     return render_template('profile.html')
+
+@app.route('/like/<video_id>')
+@check_logged_in
+def like(video_id):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM videos WHERE video_id = %s", [video_id])
+    video_details = cur.fetchone()
+    
+    likes = video_details['num_likes']
+    likes+=1
+
+    result_likes = cur.execute("SELECT * FROM likes WHERE client_id = %s AND video_id = %s", ([session['client_id']], [video_id]))
+    if result_likes == 0:
+        cur.execute("UPDATE videos SET num_likes = %s WHERE video_id = %s", (likes, video_id))
+        cur.execute("INSERT INTO likes(video_id, client_id) VALUES (%s, %s)", (video_id, session['client_id']))
+        mysql.connection.commit()
+    
+    cur.close()
+
+    return redirect(url_for('video', video_id=video_details['video_id']))
+
+def check_is_admin(arg):
+    @wraps(arg)
+    def wrap(*args, **kwargs):
+        if 'is_admin' in session:
+            return arg(*args, **kwargs)
+        else:
+            flash('You do not have admin access', 'message')
+            return redirect('/')
+    return wrap
+
+@app.route('/delete/<video_id>')
+@check_logged_in
+@check_is_admin
+def delete(video_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM videos WHERE video_id = %s", [video_id])
+    mysql.connection.commit()
+    cur.close()
+
+    return redirect(url_for('homepage'))
+
+@app.route('/ban/<client_id>')
+@check_logged_in
+@check_is_admin
+def ban(client_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM clients WHERE client_id = %s", [client_id])
+    cur.execute("INSERT INTO banned_users(admin_id, client_id) VALUES (%s, %s)", (session['admin_id'], session['client_id']))
+    mysql.connection.commit()
+    cur.close()
+    
+    return redirect(url_for('homepage'))
+
+
 
 if __name__ == "__main__":
     app.secret_key = "WBDJSBALFkjdabd"
